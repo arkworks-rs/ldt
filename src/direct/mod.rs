fb986e7c75fcf16ff7e387abf8c64cc2cc5037cc
Tom Shen <tomshen@berkeley.edu>
2021-05-04 16:20:09 -0700

    add test direct ldt + one bug fix

--- src/direct/mod.rs
+++ src/direct/mod.rs
@@ -21,8 +21,8 @@ impl<F: PrimeField> DirectLDT<F> {
                                                                     degree_bound: usize) -> DensePolynomial<F> {
        let mut evals = Evaluations::from_vec_and_domain(evaluations, domain);
         let mut poly = evals.interpolate();
-        // trim to degree bound
-        poly.coeffs.truncate(degree_bound);
+        // trim higher degree
+        poly.coeffs.truncate(degree_bound+1);
         poly
     }
 
@@ -30,11 +30,40 @@ impl<F: PrimeField> DirectLDT<F> {
     ///
     /// Verifier sample one element from domain and get its evaluation. Check if that evaluation
     /// agrees with low-degree polynomial.
-    pub fn verify_low_degree_single_round<D: EvaluationDomain<F>>(sampled_domain_element: F,
-                                                     sampled_evaluation_element: F,
-                                                     low_degree_interpolated_polynomial: &DensePolynomial<F>) -> bool {
-        return low_degree_interpolated_polynomial.evaluate(&sampled_domain_element) == sampled_evaluation_element;
+    pub fn verify_low_degree_single_round(sampled_domain_element: F,
+                                          sampled_evaluation_element: F,
+                                          coefficients: &DensePolynomial<F>) -> bool {
+        return coefficients.evaluate(&sampled_domain_element) == sampled_evaluation_element;
     }
 
 }
 
+#[cfg(test)]
+mod tests{
+    use ark_std::test_rng;
+    use ark_poly::univariate::DensePolynomial;
+    use ark_poly::{UVPolynomial, Radix2EvaluationDomain, EvaluationDomain};
+    use ark_test_curves::bls12_381::Fr;
+    use crate::direct::DirectLDT;
+
+    #[test]
+    fn test_direct_ldt(){
+        let degree = 51;
+
+        let mut rng = test_rng();
+        let poly = DensePolynomial::<Fr>::rand(degree, &mut rng);
+        let domain = Radix2EvaluationDomain::new(64).unwrap();
+        let evaluations = poly.evaluate_over_domain(domain);
+
+        let low_degree_poly = DirectLDT::generate_low_degree_coefficients(domain.clone(),
+                                                                evaluations.evals.to_vec(), degree);
+
+        let sampled_element = domain.element(15);
+        let sampled_evaluation = evaluations.evals[15];
+
+        assert!(DirectLDT::verify_low_degree_single_round(sampled_element,
+                                                          sampled_evaluation,
+                                                          &low_degree_poly))
+
+    }
+}
