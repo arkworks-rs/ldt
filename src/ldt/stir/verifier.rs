e4d6730760c5ee12cd753866e223deb574e32ca1
Andrew Z <zitek936@gmail.com>
2024-07-18 14:59:08 +0200

    chkpt

--- src/ldt/stir/verifier.rs
+++ src/ldt/stir/verifier.rs
@@ -72,29 +72,13 @@ where
             proof.clone(),
         );
         for round_proof in &proof.rounds {
-            if !round_proof.is_final_round {
-                let round_result = self.round(proof, round_proof, state);
-                if round_result.is_none() {
-                    return false;
-                }
-                state = round_result.unwrap();
+            let round_result = self.round(proof, round_proof, state);
+            if round_result.is_none() {
+                return false;
             }
+            state = round_result.unwrap();
         }
-
-        // Step 3: Randomness indices
-        let randomness_indices: Vec<usize> = state.randomness_indices();
-
-        // Step 4: Proof of work
-        if !state.verify_proof_of_work(proof) {
-            return false;
-        }
-
-        // Step 4: Folded answers
-        let oracle_answers = proof.rounds.last().unwrap().challenge_values.clone();
-        let folded_answers = state.folded_evaluations(randomness_indices, oracle_answers);
-        folded_answers
-            .into_iter()
-            .all(|(point, value)| proof.rounds.last().unwrap().coeff.evaluate(&point) == value)
+        true
     }
 }
 
@@ -114,47 +98,68 @@ where
         round_proof: &STIRProofRound<F, M, S>,
         mut state: STIRVerifierState<F, M, S>,
     ) -> Option<STIRVerifierState<F, M, S>> {
-        // Step 1: handle randomness
-        let (out_of_domain_randomness, comb_randomness, folding_randomness, randomness_indices) =
-            state.randomness(
-                round_proof.commitment_digest.clone(),
-                round_proof.out_of_domain_evaluations.clone(),
+        if !round_proof.is_final_round {
+            // Step 1: handle randomness
+            let (out_of_domain_randomness, comb_randomness, folding_randomness, randomness_indices) =
+                state.randomness(
+                    round_proof.commitment_digest.clone(),
+                    round_proof.out_of_domain_evaluations.clone(),
+                );
+
+            // Step 2: proof of work
+            if !state.verify_proof_of_work(proof) {
+                return None;
+            }
+
+            // Step 3: more randomness
+            let shake_randomness = state.sponge_squeeze();
+
+            // Step 4: quotient answers
+            let quotient_answers: Vec<(F, F)> = state.quotient_answers(
+                &round_proof.challenge_values,
+                &out_of_domain_randomness,
+                &round_proof.out_of_domain_evaluations,
+                &randomness_indices,
             );
 
-        // Step 2: proof of work
-        if !state.verify_proof_of_work(proof) {
-            return None;
+            // Step 5: verify quotient answers
+            if !round_proof.verify_quotient_answers(&quotient_answers, &shake_randomness) {
+                return None;
+            }
+            return Some(STIRVerifierState {
+                comb_randomness: comb_randomness.clone(),
+                config: self.config.clone(),
+                domain_gen: state.domain_gen * state.domain_gen,
+                domain_offset: state.domain_offset * state.domain_offset * state.root_of_unity,
+                domain_size: state.domain_size / 2,
+                folding_randomness: folding_randomness,
+                interpolating_coeff: round_proof.coeff.clone(),
+                proof: state.proof,
+                quotient_set: quotient_answers.into_iter().map(|(x, _)| x).collect(),
+                root_of_unity: state.root_of_unity,
+                round_num: state.round_num + 1,
+                sponge: state.sponge,
+            });
+        } else {
+            // Step 3: Randomness indices
+            let randomness_indices: Vec<usize> = state.randomness_indices();
+
+            // Step 4: Proof of work
+            if !state.verify_proof_of_work(proof) {
+                return None;
+            }
+
+            // Step 4: Folded answers
+            let oracle_answers = proof.rounds.last().unwrap().challenge_values.clone();
+            let folded_answers = state.folded_evaluations(randomness_indices, oracle_answers);
+            if !folded_answers
+                .into_iter()
+                .all(|(point, value)| proof.rounds.last().unwrap().coeff.evaluate(&point) == value)
+            {
+                return None;
+            }
         }
 
-        // Step 3: more randomness
-        let shake_randomness = state.sponge_squeeze();
-
-        // Step 4: quotient answers
-        let quotient_answers: Vec<(F, F)> = state.quotient_answers(
-            &round_proof.challenge_values,
-            &out_of_domain_randomness,
-            &round_proof.out_of_domain_evaluations,
-            &randomness_indices,
-        );
-
-        // Step 5: verify quotient answers
-        if !round_proof.verify_quotient_answers(&quotient_answers, &shake_randomness) {
-            return None;
-        }
-
-        Some(STIRVerifierState {
-            comb_randomness: comb_randomness.clone(),
-            config: self.config.clone(),
-            domain_gen: state.domain_gen * state.domain_gen,
-            domain_offset: state.domain_offset * state.domain_offset * state.root_of_unity,
-            domain_size: state.domain_size / 2,
-            folding_randomness: folding_randomness,
-            interpolating_coeff: round_proof.coeff.clone(),
-            proof: state.proof,
-            quotient_set: quotient_answers.into_iter().map(|(x, _)| x).collect(),
-            root_of_unity: state.root_of_unity,
-            round_num: state.round_num + 1,
-            sponge: state.sponge,
-        })
+        Some(state)
     }
 }
