6caa96e361f15ecb042967233582b2dd269a6b2d
Andrew Z <zitek936@gmail.com>
2024-07-18 16:51:35 +0200

    more cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod crypto;
 pub mod direct;
 pub mod domain;
-pub mod fri;
 pub mod ldt;
 pub mod poly_utils;
 pub mod statement;
--- src/statement/mod.rs
+++ src/statement/mod.rs
@@ -1,9 +1,8 @@
-use ark_crypto_primitives::{merkle_tree::Config as MerkleConfig, sponge::CryptographicSponge};
-use ark_ff::FftField;
+use ark_crypto_primitives::merkle_tree::Config as MerkleConfig;
 
 pub mod single;
 
-pub trait Statement<F: FftField, M: MerkleConfig, S: CryptographicSponge> {
+pub trait Statement<M: MerkleConfig> {
     type Argument;
     fn new(argument: Self::Argument) -> Self;
     fn commitment_digest(&self) -> M::InnerDigest;
